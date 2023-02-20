980925c76a8b660587c14f332e0bea200216a62a
unknown <glodybulambo@gmail.com>
2023-02-20 03:16:36 +0200

    Javascript, Assignment 5

--- Javascript/Assignment5/beanCounting.js
+++ Javascript/Assignment5/beanCounting.js
@@ -9,3 +9,16 @@ const countBs = (string) => {
     return countBs
 }
 console.log(countBs("Bisimwa"))
+
+const countChar = (string,char) => {
+    const stringLength = string.length
+    let countChar=0
+    for (let i=0; i<stringLength; i++) {
+        if (string[i] === char){
+            countChar += 1
+        }
+    }
+    return countChar
+}
+
+console.log(countChar("Bisimwa",'i'))
---
+++ Javascript/Assignment5/divisibleBySix.js
@@ -0,0 +1,8 @@
+const divisibleBySix = (number) => {
+    if (number % 6 === 0) {
+        return true
+    }
+    return false
+}
+
+console.log(divisibleBySix(11))
