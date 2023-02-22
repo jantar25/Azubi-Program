b19fd65332b9a72a73c174ea029e24fad952d6f3
unknown <glodybulambo@gmail.com>
2023-02-22 09:34:18 +0200

    Javascript, Assignment 6,sum of range

---
+++ Javascript/Assignment7/sumOfRange.js
@@ -0,0 +1,27 @@
+
+const range = (start,end,step) => {
+    const array = []
+    if (step) {
+        for (let i=start;i<=end;i+=step) {
+            array.push(i)
+        } 
+    } else {
+        for (let i=start;i<=end;i++){
+            array.push(i)
+        }
+    }
+
+    return array
+}
+
+const sum = (array) => {
+    let sumation = 0
+    for (let i=0;i<array.length;i++) {
+        sumation += array[i]
+    }
+
+    return sumation
+}
+
+console.log(range(1,10))
+console.log(sum([2,4,5,9,0,-6]))
