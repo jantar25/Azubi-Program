93b09f0348cbd91616bec912c80e90bea194fd0e
unknown <glodybulambo@gmail.com>
2023-02-27 22:22:21 +0200

    assignment 9,done

--- Javascript/Assignment9/azubiAuto.js
+++ Javascript/Assignment9/azubiAuto.js
@@ -6,7 +6,6 @@ const clientArray = [
 ];
 
 const tableBody = document.querySelector('#table-body')
-
 tableBody.innerHTML = clientArray.map(array => `
     <tr>
         <td>${array.company}</td>
@@ -14,3 +13,33 @@ tableBody.innerHTML = clientArray.map(array => `
         <td>${array.country}</td>
     </tr>
 `).join('')
+
+const runEvent = (e) => {
+    e.preventDefault()
+    const company = document.querySelector('#company')
+    const contact = document.querySelector('#contact')
+    const country = document.querySelector('#country')
+    const newObject = {
+        company:company.value,
+        contact:contact.value,
+        country:country.value
+    }
+    clientArray.push(newObject)
+    tableBody.innerHTML = clientArray.map(array => `
+    <tr>
+        <td>${array.company}</td>
+        <td>${array.contact}</td>
+        <td>${array.country}</td>
+    </tr>
+`).join('')
+
+    company.value = ''
+    contact.value = ''
+    country.value = ''
+}
+
+const form = document.querySelector('form')
+form.addEventListener('submit',runEvent)
+
+
+
