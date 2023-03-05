9ea8a95d35c6ea38f851464207663a112a9f1d41
unknown <glodybulambo@gmail.com>
2023-03-05 02:59:29 +0200

    fetching data with promises done

--- Javascript/Assignment10/restApi.js
+++ Javascript/Assignment10/restApi.js
@@ -3,7 +3,22 @@ const tableBody = document.querySelector('#table-body')
 const button = document.querySelector('#fetchBtn')
 
 const fetchData = () => {
-    console.log(tableBody)
+    fetch('https://reqres.in/api/users?page=2',{
+        headers: { 'content-Type': 'application/json'},
+        method: 'GET'
+}).then((response) => response.json())
+  .then((response) => tableBody.innerHTML = response.data.map(array => `
+    <tr>
+        <td>
+            <img src=${array.avatar} alt='Avatar' />
+        </td>
+        <td>${array.first_name}</td>
+        <td>${array.last_name}</td>
+        <td>${array.email}</td>
+    </tr>
+`).join(''))
+  .catch((error) => console.log(error))
+    
 }
 
 button.addEventListener('click',fetchData)
