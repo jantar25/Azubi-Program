0aa72db5fa7196dc986087616d312efbb93ebfee
unknown <glodybulambo@gmail.com>
2023-03-05 02:28:01 +0200

    fetch project setup

---
+++ Javascript/Assignment10/restApi.js
@@ -0,0 +1,9 @@
+
+const tableBody = document.querySelector('#table-body')
+const button = document.querySelector('#fetchBtn')
+
+const fetchData = () => {
+    console.log(tableBody)
+}
+
+button.addEventListener('click',fetchData)
