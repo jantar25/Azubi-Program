3d5f00b408d23b8bae49bf8700ae70be56e3ff8b
unknown <glodybulambo@gmail.com>
2023-03-05 03:18:32 +0200

    fetching data with async&await done

--- Javascript/Assignment10/restApi.js
+++ Javascript/Assignment10/restApi.js
@@ -2,7 +2,8 @@
 const tableBody = document.querySelector('#table-body')
 const button = document.querySelector('#fetchBtn')
 
-const fetchData = () => {
+// PROMISES
+const fetchDataWithPromise = () => {
     fetch('https://reqres.in/api/users?page=2',{
         headers: { 'content-Type': 'application/json'},
         method: 'GET'
@@ -17,8 +18,32 @@ const fetchData = () => {
         <td>${array.email}</td>
     </tr>
 `).join(''))
-  .catch((error) => console.log(error))
-    
+  .catch((error) => console.log(error))  
 }
 
-button.addEventListener('click',fetchData)
+// ASYNC/AWAIT
+const fetchDataWithAsyncAwait = async () => {
+    try {
+        const rawData = await fetch('https://reqre.in/api/users?page=2',{
+            headers: { 'content-Type': 'application/json'},
+            method: 'GET'
+        })
+        const readabledata = await rawData.json()
+        tableBody.innerHTML = readabledata.data.map(array => `
+        <tr>
+            <td>
+                <img src=${array.avatar} alt='Avatar' />
+            </td>
+            <td>${array.first_name}</td>
+            <td>${array.last_name}</td>
+            <td>${array.email}</td>
+        </tr>
+    `).join('')
+    } catch (error) {
+        tableBody.innerHTML = `<h2>${error}</h2>`
+        tableBody.style.color = 'red'
+        console.log(error)
+    }
+}
+
+button.addEventListener('click',fetchDataWithAsyncAwait)
