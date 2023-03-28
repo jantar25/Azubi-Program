afeff80e015465bb4c36b4490fae70871f970a3a
unknown <glodybulambo@gmail.com>
2023-03-28 20:45:47 +0200

    Css condition done

--- React/cards-components/src/Components/CardsItems.jsx
+++ React/cards-components/src/Components/CardsItems.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './CardsItems.css'
 
 import Item from './Item/Item'
 import data from '../data'
 
 const CardsItems = () => {
   return (
-    <div>
+    <div className='cards-container'>
         {data.map(card => (
             <div key={card.id}>
                 <Item card={card} />
--- React/cards-components/src/Components/Item/Item.jsx
+++ React/cards-components/src/Components/Item/Item.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import './Item.css'
+
 const Item = ({card}) => {
   return (
-    <div>
-        {card.title}
+    <div 
+    className={`card-conatiner ${card.title === 'SEDANS'?
+    'sedans' : card.title === 'SUVS'? 'suvs' : 'luxury'}`} >
+        <img src={card.icon} alt={card.title} />
+        <h2 className='title'>{card.title}</h2>
+        <p className='description'>{card.description}</p>
+        <button>Learn More</button>
     </div>
   )
 }
