95d58818bfbdaf0a52e12bc33dce4a0780598440
unknown <glodybulambo@gmail.com>
2023-04-07 19:34:30 +0200

    Project search bar design

--- Javascript/Assignment8/groupClass.js
+++ Javascript/Assignment8/groupClass.js
@@ -1,9 +1,25 @@
 class Group {
     constructor() {
-        this.group = {};
+        this.group = [];
       }
 
-    add() {
-        
+    add(member) {
+      const newGroup = [...this.group,this.group.concat(member)]
+        return newGroup;
+    }
+
+    delete(member) {
+      return this.group.map(item => item)
+    }
+
+    has() {
+
+    }
+
+    from () {
+
     }
 }
+
+console.log(new Group().add('Jantar'))
+console.log(new Group().delete('Glody'))
