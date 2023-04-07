4d3dff79a598820e93078bbe45a3532ba9b83616
unknown <glodybulambo@gmail.com>
2023-04-07 20:02:34 +0200

    NavBar done

--- Javascript/Project/header.js
+++ Javascript/Project/header.js
@@ -1,12 +1,21 @@
 let menuBtn = document.querySelector("#menu")
 let searchBtn = document.querySelector("#searchBtn") 
 let navbar = document.querySelector(".navbar")
-let searchForm = document.querySelector("#searchForm")
+let searchForm = document.querySelector(".searchForm")
+let toggleSearchBar = false
 
+// Toggle Search Bar
 const toggleSearch = () => {
-    searchForm.classList.toggle('active');
-    console.log('click')
+    if(!toggleSearchBar) {
+        searchForm.style.display = 'flex';
+        toggleSearchBar = true;
+    } else {
+        searchForm.style.display = 'none';
+        toggleSearchBar = false;
+    }  
 }
+
+// toggle Menu Bar
 const toggleMenu = () => {
     menuBtn.classList.toggle('fa-times');
     navbar.classList.toggle('active');
@@ -15,6 +24,9 @@ const toggleMenu = () => {
 window.onscroll = () => {
     menuBtn.classList.remove('fa-times');
     navbar.classList.remove('active');
+    searchForm.style.display = 'none';
+    toggleSearchBar = false;
 }
+
 menuBtn.addEventListener('click',toggleMenu)
 searchBtn.addEventListener('click',toggleSearch)
