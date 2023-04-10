ac4741221139ad2ac1a44ba6828eaaf041f545b4
unknown <glodybulambo@gmail.com>
2023-04-10 12:35:32 +0200

    Our dishes done

--- Javascript/Project/Javascript/dishes.js
+++ Javascript/Project/Javascript/dishes.js
@@ -1,22 +1,49 @@
-const burger = './Assets/burger.jpg'
+const hamburger = './Assets/hamburger.png'
+const cake = './Assets/cake.png'
+const coffee = './Assets/coffee.png'
+const smoothie = './Assets/smoothie.png'
+const iceCream = './Assets/ice-cream.png'
+const pizza = './Assets/pizza.png'
 const dishesContainer = document.querySelector('.dishes')
 
 
 const dishes = [
     {
         id:1,
-        iconImage:burger,
+        iconImage:hamburger,
         title:'Tasty Burger',
         description:'simply and double burger sandwich consisting of fillings—usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll',
-        image:burger
     },
     {
         id:2,
-        iconImage:burger,
-        title:'Tasty Burger',
-        description:'simply and double burger sandwich consisting of fillings—usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll',
-        image:burger
-    }
+        iconImage:pizza,
+        title:'Tasty Pizza',
+        description:'dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients',
+    },
+    {
+        id:3,
+        iconImage:iceCream,
+        title:'Ice Cream',
+        description:'frozen dessert made from milk or cream and flavoured with a sweetener and a spice, such as cocoa or vanilla, or with fruit such as strawberries or peaches',
+    },
+    {
+        id:4,
+        iconImage:smoothie,
+        title:'Smoothie',
+        description:'beverage made by puréeing ingredients in a blender.',
+    },
+    {
+        id:5,
+        iconImage:coffee,
+        title:'Coffee',
+        description:'beverage prepared from roasted coffee beans. Darkly colored, bitter, and slightly acidic',
+    },
+    {
+        id:6,
+        iconImage:cake,
+        title:'Cakes',
+        description:'confection made from flour, sugar, and other ingredients, and is usually baked',
+    },
 ]
 
 dishesContainer.innerHTML = dishes.map(dish => `
