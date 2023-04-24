9d25e628b1ec2ba114bd131a20b0a60d7c4b89f0
unknown <glodybulambo@gmail.com>
2023-04-24 04:02:00 +0200

    gift_card done

--- React/gift_card/src/App.js
+++ React/gift_card/src/App.js
@@ -1,11 +1,49 @@
+import { useState } from "react";
 
+export default function App() {
+  const [giftCard, setGiftCard] = useState(
+    {
+        firstName: "Jennifer",
+        lastName: "Smith",
+        text: "Free dinner for 4 guests",
+        valid: true,
+        instructions: "To use your coupon, click the button below.",
+    }
+  );
 
-function App() {
+  function spendGiftCard() {
+    setGiftCard((prevState) => {
+      return {
+        ...prevState,
+        text: "Your coupon has been used.",
+        valid: false,
+        instructions: "Please visit our restaurant to renew you gift card.",
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      GIFT
+    <div style={{padding: '40px'}}>
+      <h1>
+        Gift Card Page
+      </h1>
+      <h2>
+        Customer: {giftCard.firstName} {giftCard.lastName}
+      </h2>
+      <h3>
+        {giftCard.text}
+      </h3>
+      <p>
+        {giftCard.instructions}
+      </p>
+      {
+        giftCard.valid && (
+          <button onClick={spendGiftCard}>
+            Spend Gift Card
+          </button>
+        )
+      }
     </div>
   );
 }
 
-export default App;
